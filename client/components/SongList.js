44d8a949bf8072734c1dfc1495ba335e0b342d35
Venus Ang <venusang24@gmail.com>
2019-01-30 11:56:53 -0800

    Setting up the client-side component SongList

---
+++ client/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; //making this class-based because it will contain helper methods
+
+class SongList extends Component {
+  render() {
+    return(
+      <div>
+        SongList
+      </div>
+    );
+  }
+}
+
+export default SongList;
